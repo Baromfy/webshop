d5c2cd1f6f4bdffd11c6e4e96fab52361c796c25
Baromfy <molnarabel2000@gmail.com>
2025-01-25 10:57:19 +0100

    finish profuct component

--- onlineShop/src/app/product/product.component.ts
+++ onlineShop/src/app/product/product.component.ts
@@ -4,11 +4,12 @@ import { Product } from '../models/product.model';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule],
   styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
